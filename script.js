568b770d85082f0c0e784a75dcd4cdaf41cab94f
Glitch (hello-webpage) <none>
2021-02-19 16:11:11 +0000

    🚶🚠 Checkpoint ./script.js:11532741/765

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-// bg animation
-gsap.from(".bg",{scale:1.2})
+// initation
+gsap.fromTo(".bg",{duration:3,scale:1.5},{scale:1.1})
+gsap.from("#apple",{duration:2,scale:0.95})
 
+
+// mouse-based movements //
 var centerX = window.innerWidth / 2;
 var centerY= window.innerHeight / 2;
 
 $("body").mousemove(function(e) {
     // console.log(e.pageX,e.pageY);
   var xDiff = e.pageX-centerX;
-  var mapped_xDff = map(xDiff,-)
+  var yDiff = e.pageY-centerY;
+  var mapped_xDiff = map(xDiff,-600,600,1.09,1);
+  
+  // bg movement
+  gsap.to(".bg",{scale: mapped_xDiff})
+  
+  // apple movement
+  var apple_xDiff = map(xDiff,-600,600,3,-3);
+  var apple_yDiff = map(yDiff,-800,800,3,-3);
+  gsap.to("#apple",{y:apple_xDiff,x:apple_yDiff});
 })
