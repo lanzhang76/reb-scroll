e2c9ce44518b1c71f60bc10322a13c9424b49610
Glitch (hello-webpage) <none>
2021-02-19 19:48:14 +0000

    📰😬 Checkpoint ./script.js:11532741/12163

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
-let init = true;
+let initiate = true;
 
-function init() {
+function initAnimation() {
   // initation
-    gsap.fromTo(".bg", { duration: 3, scale: 1.5 }, { scale: 1.2 });
-    gsap.from("#apple", { duration: 4, scale: 0.95 });
-    gsap.from(".animate-leaf", {
-      duration: 2,
-      scale: 0.5,
-      opacity: 0.3,
-      x: 0,
-      y: "-=3",
-      transformOrigin: "50% top"
-    });
-    const leaftl = gsap
-      .timeline()
-      .to(
-        ["#animate-leaf-bottom"],
-        { duration: 2, x: "+=15", y: "+=15" },
-        "-=0.5"
-      )
-      .to(["#animate-leaf-left"], { duration: 2, x: "-=5", y: "+=05" }, "-=0.5")
-      .to(["#animate-leaf-left"], { duration: 2, x: 0, y: 0 });
-    init = false;
+  gsap.fromTo(".bg", { duration: 3, scale: 1.5 }, { scale: 1.2 });
+  gsap.from("#apple", { duration: 4, scale: 0.95 });
+  gsap.from(".animate-leaf", {
+    duration: 2,
+    scale: 0.5,
+    opacity: 0.3,
+    x: 0,
+    y: "-=3",
+    transformOrigin: "50% top"
+  });
+  const leaftl = gsap
+    .timeline()
+    .to(
+      ["#animate-leaf-bottom"],
+      { duration: 2, x: "+=15", y: "+=15" },
+      "-=0.5"
+    )
+    .to(["#animate-leaf-left"], { duration: 2, x: "-=5", y: "+=05" }, "-=0.5")
+    .to(["#animate-leaf-left"], { duration: 2, x: 0, y: 0 });
+  initiate = false;
 }
 
-function animate(){
+function animate() {
   // mouse-based movements //
-    var centerX = window.innerWidth / 2;
-    var centerY = window.innerHeight / 2;
+  var centerX = window.innerWidth / 2;
+  var centerY = window.innerHeight / 2;
 
-    $("body").mousemove(function(e) {
-      // console.log(e.pageX,e.pageY);
-      var xDiff = e.pageX - centerX;
-      var yDiff = e.pageY - centerY;
-      var mapped_xDiff = map(xDiff, -600, 600, 1.09, 1);
+  $("body").mousemove(function(e) {
+    // console.log(e.pageX,e.pageY);
+    var xDiff = e.pageX - centerX;
+    var yDiff = e.pageY - centerY;
+    var mapped_xDiff = map(xDiff, -600, 600, 1.09, 1);
 
-      // bg movement
-      gsap.to(".bg", { scale: mapped_xDiff });
+    // bg movement
+    gsap.to(".bg", { scale: mapped_xDiff });
 
-      // apple movement
-      var apple_xDiff = map(xDiff, -600, 600, 3, -3);
-      var apple_yDiff = map(yDiff, -800, 800, 3, -3);
-      gsap.to("#apple", { y: apple_xDiff, x: apple_yDiff });
+    // apple movement
+    var apple_xDiff = map(xDiff, -600, 600, 3, -3);
+    var apple_yDiff = map(yDiff, -800, 800, 3, -3);
+    gsap.to("#apple", { y: apple_xDiff, x: apple_yDiff });
 
-      // leaves movement
-      var apple_xDiff = map(xDiff, -600, 600, 5, -5);
-      var apple_yDiff = map(yDiff, -800, 800, 5, -5);
-      gsap.to(".animate-leaf", { y: apple_yDiff, x: apple_xDiff });
+    // leaves movement
+    var apple_xDiff = map(xDiff, -600, 600, 5, -5);
+    var apple_yDiff = map(yDiff, -800, 800, 5, -5);
+    gsap.to(".animate-leaf", { y: apple_yDiff, x: apple_xDiff });
+  });
 }
 
-                        
-setTimeInterval(1000,function(){
-                              
-if(init==true){
-      init()
-} else {
-  animate();
-}
-    })                       
 
-
+initAnimation();
+setTimeout(animate(),5000);
