edd8326c2082bebe8c65f4b74b399117db96ddd3
Glitch (hello-webpage) <none>
2021-02-23 15:58:48 +0000

    💴👶 Checkpoint ./script.js:11532741/762 ./index.html:11532741/16934 ./style.css:11532741/136

--- script.js
+++ script.js
@@ -3,42 +3,49 @@ let initiate = true;
 function initAnimation() {
   // initation
   gsap.fromTo(".bg", { duration: 5, scale: 2.2 }, { scale: 1.3 });
-  gsap.from("#apple", { duration: 4, scale: 0.95 });
-  gsap.from(".animate-leaf", {
-    duration: 2,
-    scale: 0.5,
-    opacity: 0.3,
-    x: 0,
-    y: "-=3",
-    transformOrigin: "50% top"
-  });
-  gsap.from(".animate-flower", {
-    duration: 5,
-    scale: 0.8,
-    opacity: 0.8,
-    rotate: "-=25",
-    transformOrigin: "50% 50%"
-  });
-  gsap.from(".animate-bud", {
-    duration: 5,
-    scale: 1.2,
-    opacity: 0.8,
-    rotate: "+=25",
-    transformOrigin: "50% 50%"
-  });
+  const branchtl = gsap.timeline()
+  .from(".branch", { duration: 2, scale: 0.95, x:15 ,y:5,rotation: "-=2" })
+  .to(".branch",{duration:0.5,y:-2})
+  .to(".branch",{duration:0.5,y:2})
+  .to(".branch",{duration:0.5,y:-1})
+  .to(".branch",{duration:0.5,y:1,})
+  .to(".branch",{duration:0.5,y:0,})
+  .to("#branch-apple",{duration:1,x:-10,y:-4,scale:1.02});
+//   gsap.from(".animate-leaf", {
+//     duration: 2,
+//     scale: 0.5,
+//     opacity: 0.3,
+//     x: 0,
+//     y: "-=3",
+//     transformOrigin: "50% top"
+//   });
+//   gsap.from(".animate-flower", {
+//     duration: 5,
+//     scale: 0.8,
+//     opacity: 0.8,
+//     rotate: "-=25",
+//     transformOrigin: "50% 50%"
+//   });
+//   gsap.from(".animate-bud", {
+//     duration: 5,
+//     scale: 1.2,
+//     opacity: 0.8,
+//     rotate: "+=25",
+//     transformOrigin: "50% 50%"
+//   });
 
-  const leaftl = gsap
-    .timeline()
-    .to(
-      ["#animate-leaf-bottom"],
-      { duration: 2, x: "-=15", y: "+=15" },
-      "-=0.5"
-    )
-    .to(["#animate-leaf-bottom"], { duration: 2, x: 0, y: 0 })
-    .to(["#animate-leaf-left"], { duration: 2, x: "-=5", y: "+=5" }, "-=0.5")
-    .to(["#animate-leaf-left"], { duration: 2, x: 0, y: 0 })
-    .to(["#animate-leaf-right"], { duration: 2, x: "-=10", y: "-=5" }, "-=1")
-    .to(["#animate-leaf-top"], { duration: 2, rotate: "+=3" }, "-=2");
+//   const leaftl = gsap
+//     .timeline()
+//     .to(
+//       ["#animate-leaf-bottom"],
+//       { duration: 2, x: "-=15", y: "+=15" },
+//       "-=0.5"
+//     )
+//     .to(["#animate-leaf-bottom"], { duration: 2, x: 0, y: 0 })
+//     .to(["#animate-leaf-left"], { duration: 2, x: "-=5", y: "+=5" }, "-=0.5")
+//     .to(["#animate-leaf-left"], { duration: 2, x: 0, y: 0 })
+//     .to(["#animate-leaf-right"], { duration: 2, x: "-=10", y: "-=5" }, "-=1")
+//     .to(["#animate-leaf-top"], { duration: 2, rotate: "+=3" }, "-=2");
 
   initiate = false;
 }
@@ -63,9 +70,9 @@ function animate() {
     
     
     // apple movement
-    var apple_xDiff = map(xDiff, -600, 600, 3, -3);
-    var apple_yDiff = map(yDiff, -800, 800, 3, -3);
-    gsap.to("#branch-apple", { y: apple_xDiff, x: apple_yDiff});
+    var apple_xDiff = map(xDiff, -600, 600, 1, -2);
+    var apple_yDiff = map(yDiff, -800, 800, 0.5, -0.5);
+    gsap.to("#branch-apple", { y: apple_xDiff, rotation: apple_yDiff});
 
     // leaves movement
     // var apple_xDiff = map(xDiff, -600, 600, 5, -5);
