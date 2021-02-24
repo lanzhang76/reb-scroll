d7f16638134da34fee1a91d1186a8033dfc4c050
Glitch (hello-webpage) <none>
2021-02-24 14:40:26 +0000

    🏆🏛 Checkpoint ./script.js:11532741/2322 ./index.html:11532741/4

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-function scrollAction(t_id){
-  let trigger = 
-    {
-      scroller: ".content",
-      trigger: t_id,
-      start: "top 500px",
-      end: "bottom 100px",
-      markers: true,
-      pinSpacing: false,
-      pin: true,
-      scrub: true,
-      onLeave: () => {
-        gsap.to(t_id, {
-          duration:0.2,
-          opacity: 0
-        });
-      }
-  }
-    return trigger
+function scrollAction(t_id) {
+  let trigger = {
+    scroller: ".content",
+    trigger: t_id,
+    start: "top 500px",
+    end: "bottom 100px",
+    // markers: true,
+    pinSpacing: false,
+    pin: true,
+    scrub: true,
+    onLeave: () => {
+      gsap.to(t_id, {
+        duration: 0.2,
+        opacity: 0
+      });
+    }
+  };
+  return trigger;
 }
-
 
 gsap.fromTo(
   "#modal1",
@@ -26,7 +24,7 @@ gsap.fromTo(
     opacity: 0
   },
   {
-    scrollAction("#modal1"),
+    scrollTrigger: scrollAction("#modal1"),
     opacity: 1
   }
 );
@@ -37,23 +35,7 @@ gsap.fromTo(
     opacity: 0
   },
   {
-    scrollTrigger: {
-      scroller: ".content",
-      trigger: "#modal2",
-      start: "top 500px",
-      end: "bottom 100px",
-      markers: true,
-      pinSpacing: false,
-      pin: true,
-      scrub: true,
-      onLeave: () => {
-        gsap.to("#modal2", {
-          duration:0.2,
-          opacity: 0
-        });
-        console.log("leave");
-      }
-    },
+    scrollTrigger: scrollAction("#modal2"),
     opacity: 1
   }
 );
