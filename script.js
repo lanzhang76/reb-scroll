a5273563a4fcc0a10c0fdb8cbfe24be8a7451e75
Glitch (hello-webpage) <none>
2021-02-24 14:28:28 +0000

    🦃🐈 Checkpoint ./script.js:11532741/3041 ./style.css:11532741/413

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
+function scrollAction(t_id){
+  let trigger = 
+    {
+      scroller: ".content",
+      trigger: t_id,
+      start: "top 500px",
+      end: "bottom 100px",
+      markers: true,
+      pinSpacing: false,
+      pin: true,
+      scrub: true,
+      onLeave: () => {
+        gsap.to(t_id, {
+          duration:0.2,
+          opacity: 0
+        });
+      }
+  }
+    return trigger
+}
+
+
 gsap.fromTo(
   "#modal1",
   {
     opacity: 0
   },
   {
-    scrollTrigger: {
-      scroller: ".content",
-      trigger: "#modal1",
-      start: "top 400px",
-      end: "bottom 100px",
-      markers: true,
-      pinSpacing: false,
-      pin: true,
-      scrub: true
-    },
+    scrollAction("#modal1"),
     opacity: 1
   }
 );
 
-// gsap.from("#modal2", {
-//   scrollTrigger: "#modal2",
-//   start: "top bottom",
-//   y: 100,
-//   opacity: 0
-// });
+gsap.fromTo(
+  "#modal2",
+  {
+    opacity: 0
+  },
+  {
+    scrollTrigger: {
+      scroller: ".content",
+      trigger: "#modal2",
+      start: "top 500px",
+      end: "bottom 100px",
+      markers: true,
+      pinSpacing: false,
+      pin: true,
+      scrub: true,
+      onLeave: () => {
+        gsap.to("#modal2", {
+          duration:0.2,
+          opacity: 0
+        });
+        console.log("leave");
+      }
+    },
+    opacity: 1
+  }
+);
