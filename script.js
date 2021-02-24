bf545d13f8739b100cf5b4bd819811a1a9f43ae4
Glitch (hello-webpage) <none>
2021-02-24 14:16:22 +0000

    🍱🍙 Checkpoint ./style.css:11532741/251 ./index.html:11532741/4805 ./script.js:11532741/973

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-gsap.to("#modal1", {
-  scrollTrigger: {
-    trigger: "#modal1",
-    start: 'top 200px',
-    end: 'bottom 10px',
-    markers: true,
-    // pinSpacing: false,
-    pin: true,
-    // scrub: true
+gsap.fromTo(
+  "#modal1",
+  {
+    opacity: 0
   },
-  y: -100,
-});
+  {
+    scrollTrigger: {
+      scroller: ".content",
+      trigger: "#modal1",
+      start: "top 400px",
+      end: "bottom 100px",
+      markers: true,
+      pinSpacing: false,
+      pin: true,
+      scrub: true
+    },
+    opacity: 1
+  }
+);
 
 // gsap.from("#modal2", {
 //   scrollTrigger: "#modal2",
