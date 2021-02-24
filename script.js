a77334c887e40336a09ad33e92a3cbf69b65b984
Glitch (hello-webpage) <none>
2021-02-24 13:48:55 +0000

    🐥💓 Checkpoint ./script.js:11532741/78 ./style.css:11532741/284

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// gsap.to("#modal1", {
-//   scrollTrigger: {
-//     trigger: "#modal1",
-//     start: 'top center',
-//     end: 'bottom 150px',
-//     markers: true,
-//     pinSpacing: false,
-//     pin: true,
-//     // scrub: true
-//   },
-//   y: -100,
-// });
+gsap.to("#modal1", {
+  scrollTrigger: {
+    trigger: "#modal2",
+    start: 'top 200px',
+    end: 'bottom 10px',
+    // markers: true,
+    // pinSpacing: false,
+    pin: true,
+    // scrub: true
+  },
+  y: -100,
+});
 
 // gsap.from("#modal2", {
 //   scrollTrigger: "#modal2",
